39358bdf75d26d817013ed961aa128a122595e12
5rovski <petrovski.nikola@hotmail.com>
2020-04-04 02:12:19 +0200

    student class finished

--- Student.java
+++ Student.java
@@ -1,18 +1,67 @@
 class Student {
-	String index;
-	String firstName;
-	String lastName;
-	List<Integer> grades;
+    String index;
+    String firstName;
+    String lastName;
+    List<Integer> grades;
 
-	//TODO constructor
+    //TODO constructor
+    public Student(String index, String firstName, String lastName, List<Integer> grades){
+        this.index = index;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.grades = grades;
 
-	//TODO seters & getters
+    }
+    //TODO seters & getters
+    public String getIndex(){
+        return index;
+    }
 
-	public double getAverage() {
-		//TODO
-	}
+    public String getFirstName(){
+        return firstName;
+    }
 
-	public int ECTSCredits() {
-		//TODO
-	}
+    public String getLastName(){
+        return lastName;
+    }
+
+    public List<Integer> getGrades(){
+        return grades;
+    }
+
+    public void setIndex(String index){
+        this.index = index;
+    }
+
+    public void setFirstName(String firstName){
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName){
+        this.lastName = lastName;
+    }
+
+    public void setGrades(List<Integer> grades){
+        this.grades = grades;
+    }
+
+    public double getAverage() {
+        //TODO
+        int average = 0;
+        int sum = 0;
+        for(int i=0;i<grades.size();i++){
+            sum += grades.get(i);
+        }
+        average = sum/grades.size();
+    }
+
+    public int ECTSCredits() {
+        //TODO
+        int credits = 0;
+        for(int i=0;i<grades.size();i++){
+            if(grades.get(i) > 5){
+                credits += 6;
+            }
+        }
+    }
 }
